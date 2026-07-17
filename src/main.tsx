15df405e2f078e56b9871fa76b6afc6528a3b57f
Zaky Aulia Qolbi <bangzaky0029@gmail.com>
2026-07-17 11:52:01 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '../../kalkulator-pajak-properti/src/index.css'
-import '../../kalkulator-pajak-properti/src/global.css'
-import '../../kalkulator-pajak-properti/src/App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './global.css';
+import './index.css';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
